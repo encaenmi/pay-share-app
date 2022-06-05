bfbf83a173e571e3b843be0ac593fe5b177dd727
encaenmi <ece20@georgetown.edu>
2022-06-05 14:47:56 -0500

    started with login screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login/Login';
 
-function App() {
+
+const App = () => {
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello</p>
     </div>
   );
 }
---
+++ src/components/Login/Login.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+    return(
+        <div className="login-wrapper">
+            <form>
+                <label>
+                    <p>Username</p>
+                    <input type="text" />
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input type="password" />
+                </label>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
